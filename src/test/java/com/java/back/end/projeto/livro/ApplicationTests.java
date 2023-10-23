113965b4f61c3dac7eda2c2363ae274d35de5e79
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-10-22 23:54:27 -0300

    feat: projeto criado

---
+++ src/test/java/com/java/back/end/projeto/livro/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.back.end.projeto.livro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
